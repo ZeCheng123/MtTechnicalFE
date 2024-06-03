7e0ecdf934306048db61ff152475454dfdd77fee
wu <1347757229@qq.com>
2024-06-03 14:44:26 +0800

    修复登录频繁获取手机号

--- pages/custom/login2/login.js
+++ pages/custom/login2/login.js
@@ -7,36 +7,59 @@ Page({
       checkboxValue: false,
       btnColor: '#bbd3fb', // 初始按钮颜色
       isTrue:false,
+      isPhone:false,
       dynamicCode: "",
-      code: "",
-      PhoneNumber:""
+      code: ""
     },
     onLoad(options) {
-      // const storedUserInfo = wx.getStorageSync('userInfo');
-      // if(storedUserInfo){
-      //   wx.login({
-      //     success: (res) => {
-      //       const code = res.code;
-      //       api.postWechat({"code": code,"userType": 1}).then(res => {
-      //         if (res && res.data && res.data?.token){
-      //           console.log("接口返回了 token:", res.data?.token);
-      //           app.globalData.baseInfo.token = res.data?.token;
-      //           wx.setStorage({
-      //             key:"token",
-      //             data: res.data?.token
-      //           })
-      //           wx.switchTab({
-      //             url: '/pages/custom/workbenches/workbenches'
-      //           });
-      //         }else{
-      //           console.log("未授权手机号")
-      //         }
-      //       })
-      //     },
-      //   })  
-      // }else{
-      //   console.log("不存在授权信息")
-      // }
+
+    },
+    //勾选协议
+    checkboxBtn(e)
+    {
+      var that = this;
+      let lastValue = this.data.checkboxValue;
+      if(!lastValue){
+        that.authorizedLogin()
+      }else{
+        that.setData({
+          isTrue:false
+        })
+      }
+      this.setData({
+        checkboxValue: !lastValue,
+        btnColor: !lastValue ? '#007bff' : '#bbd3fb', // 改变按钮颜色
+      });
+    },
+    authorizedLogin(){
+      var that = this;
+      wx.login({
+        success: (res) => {
+          const code = res.code;
+          api.postWechat({"code": code,"userType": 1}).then(res => {
+            if(res?.code === "success"){
+              wx.setStorageSync('UserName', res);
+              if (res && res.data && res.data?.token){
+                app.globalData.baseInfo.token = res.data?.token;
+                wx.setStorage({
+                  key:"token",
+                  data: res?.token
+                })
+              }else{
+                that.setData({
+                  isTrue:true,
+                  isPhone:true
+                })
+              }
+            }else{
+              wx.showToast({
+                title: '登录失败:'+res?.message,
+                icon:'none'
+              })
+            }
+          });
+        },
+      }) 
     },
     getPhoneNumber(e){
       // console.log("动态令牌",e.detail)  // 动态令牌
@@ -112,59 +135,44 @@ Page({
         }
       });
     },
-    checkboxBtn(e)
-    {
-      let lastValue = this.data.checkboxValue;
-      let PhoneNumber = ""
-      if(!lastValue){
-        PhoneNumber = "getPhoneNumber"
-      }else{
-        PhoneNumber = ""
-      }
-      this.setData({
-        checkboxValue: !lastValue,
-        btnColor: !lastValue ? '#007bff' : '#bbd3fb', // 改变按钮颜色
-        PhoneNumber:PhoneNumber
-      });
-      console.log("lastValue",lastValue)
-      console.log("getPhoneNumber",this.data.PhoneNumber)
-    },
-    //获取头像+名称方法
-    getUserProfile(e) {
-      var that = this;
-      if (this.data.checkboxValue) {
-        const storedUserInfo = wx.getStorageSync('userInfo');
-        if(storedUserInfo){
-          console.log("本地已存在用户信息，不需要再次获取。");
-          this.setData({
-            userInfo: storedUserInfo,
-            hasUserInfo: true,
-            isTrue: true,
-            // PhoneNumber:"getPhoneNumber"
-          });
-        } else{
-          wx.getUserProfile({
-            desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-            success: (res) => {
-              console.log("res",res)
-              this.setData({
-                userInfo: res.userInfo,
-                hasUserInfo: true,
-                isTrue:true,
-                // PhoneNumber:"getPhoneNumber"
-              });
-              wx.setStorageSync('userInfo', res.userInfo);
-            }
-          })
-        } 
-      } else {
-        // 在需要弹出提示框的地方调用该方法
-        wx.showToast({
-          title: '请先勾选同意条例',
-          icon: 'none', // 提示框图标，可选值：'success', 'loading', 'none'
-          duration: 2000 // 提示框持续时间，单位为毫秒
+   //获取头像+名称方法
+   getUserProfile(e) {
+    var that = this;
+    if (this.data.checkboxValue) {
+      const storedUserInfo = wx.getStorageSync('userInfo');
+      if(storedUserInfo){
+        console.log("本地已存在用户信息，不需要再次获取。");
+        this.setData({
+          userInfo: storedUserInfo,
+          hasUserInfo: true,
+          // PhoneNumber:"getPhoneNumber"
         });
-        // console.log('请先勾选同意条例');
+      } else{
+        wx.getUserProfile({
+          desc: '获取你的昵称、头像、地区及性别', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+          success: (res) => {
+            // console.log("res",res)
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true,
+            });
+            wx.setStorageSync('userInfo', res.userInfo);
+          }
+        })
+      } 
+      if(!that.isPhone){
+        wx.switchTab({
+          url: '/pages/custom/workbenches/workbenches'
+        });
       }
+    } else {
+      // 在需要弹出提示框的地方调用该方法
+      wx.showToast({
+        title: '请先勾选同意条例',
+        icon: 'none', // 提示框图标，可选值：'success', 'loading', 'none'
+        duration: 2000 // 提示框持续时间，单位为毫秒
+      });
+      // console.log('请先勾选同意条例');
     }
+  }
 });
